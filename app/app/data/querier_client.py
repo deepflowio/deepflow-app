ae8e776bf1d3d0147ee0c9cbbb80494e97a7072e
Liu Chao <liuchao@yunshan.net>
2023-08-10 16:59:56 +0800

    Modify prewhere to no_prewhere
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- app/app/data/querier_client.py
+++ app/app/data/querier_client.py
@@ -40,7 +40,7 @@ class Query(object):
         log.debug(
             f"Query UUID: {self.query_uuid} | Database: {self.database}@{self.host} | SQL: {self.sql}"
         )
-        url = f"http://{self.host}:{config.querier_port}/v1/query/?query_uuid={self.query_uuid}&prewhere=false"
+        url = f"http://{self.host}:{config.querier_port}/v1/query/?query_uuid={self.query_uuid}&no_prewhere=true"
         if self.debug:
             url += "&debug=true"
         data = {'db': self.database, 'sql': self.sql}
