4159f658c1d7bfd0329907a476d4974849ab17bc
taloric <jiawei@yunshan.net>
2024-04-11 18:52:58 +0800

    fix server process related to client process

--- app/app/common/disjoint_set.py
+++ app/app/common/disjoint_set.py
@@ -11,12 +11,14 @@ class DisjointSet:
     def _get(self, index: int, start_index: int) -> int:
         # use start_index avoid loop
 
+        # [-1, index, start_index] maybe root index for each tree
         if self.disjoint_set[index] in [-1, index, start_index]:
             if self.disjoint_set[index] > 0:
                 return self.disjoint_set[index]
             else:
                 return index
 
+        # find root index to do full compress
         root_index = self._get(self.disjoint_set[index], start_index)
         self.disjoint_set[index] = root_index
         return root_index
