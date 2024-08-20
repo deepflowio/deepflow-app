bf1c06359ace45b3c77c489619a015a6d6510d2c
taloric <jiawei@yunshan.net>
2024-08-20 10:17:01 +0800

    fix: ignore time cover for OTel source

--- app/app/application/l7_flow_tracing.py
+++ app/app/application/l7_flow_tracing.py
@@ -1695,10 +1695,12 @@ class ProcessSpanSet:
                     x_request_id_match = span.get_x_request_id_0() and (span.get_x_request_id_0() == client_sys_span.get_x_request_id_0()) \
                                         or (span.get_x_request_id_1() and (span.get_x_request_id_1() == client_sys_span.get_x_request_id_1()\
                                           or span.get_x_request_id_1() == client_sys_span.get_x_request_id_0()))
-                # for cross-thread span but in same trace_id/process and time range covered 
+                # for cross-thread span but in same trace_id/process and time range covered
                 if not client_syscall_match and not sys_span_matched and not x_request_id_match:
                     # same proces & time cover already find out above, at here we only find out trace_id match
-                    same_process_trace_match = span.flow["trace_id"] and span.flow["trace_id"] == client_sys_span.flow["trace_id"]
+                    same_process_trace_match = span.flow[
+                        "trace_id"] and span.flow[
+                            "trace_id"] == client_sys_span.flow["trace_id"]
 
                 if sys_span_matched:
                     mounted_info = "syscall_trace_id matched to s-p root"
@@ -2246,7 +2248,7 @@ def _connect_process_and_networks(process_roots: List[SpanNode],
         # 避免子循环多次访问字典
         ps_index = ps_parent.get_flow_index()
         ps_span_id = ps_parent.get_span_id()
-        ps_response_duration = ps_parent.get_response_duration() 
+        ps_response_duration = ps_parent.get_response_duration()
         for net_child in network_roots:
             if net_child.get_parent_id() >= 0:
                 continue
@@ -2254,15 +2256,17 @@ def _connect_process_and_networks(process_roots: List[SpanNode],
             if _same_span_set(ps_parent, net_child, 'network_span_set') \
                 or _same_span_set(ps_parent, net_child, 'process_span_set'):
                 continue
-            if net_child.agent_id == ps_parent.agent_id and not ps_parent.time_range_cover(net_child):
-                # 对同一个主机采集到的数据，不存在时差
-                continue
-            if net_child.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL and ps_parent.signal_source!= L7_FLOW_SIGNAL_SOURCE_OTEL and \
-                  net_child.get_response_duration() and ps_response_duration < net_child.get_response_duration():
-                # 如果能取到响应时长（请求响应完整），需要判断响应时长覆盖
-                # 由于 app span 的时长是在 sdk 中统计，如果发生子 span 异步完成，父 span 提前完成，子 span 时间可以大于父 span 
-                # 所以这里判断 response_duration 忽略 OTEL signal_source
-                continue
+            if net_child.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL and ps_parent.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL:
+                if net_child.agent_id == ps_parent.agent_id and not ps_parent.time_range_cover(
+                        net_child):
+                    # 对同一个主机采集到的数据，不存在时差
+                    continue
+                if net_child.get_response_duration(
+                ) and ps_response_duration < net_child.get_response_duration():
+                    # 如果能取到响应时长（请求响应完整），需要判断响应时长覆盖
+                    # 由于 app span 的时长是在 sdk 中统计，如果发生子 span 异步完成，父 span 提前完成，子 span 时间可以大于父 span
+                    # 所以这里判断 response_duration 忽略 OTEL signal_source
+                    continue
             if ps_index == net_child.get_flow_index():
                 # 共享一个 c-p, net_child parent == ps_parent 的 parent
                 continue
@@ -2285,11 +2289,14 @@ def _connect_process_and_networks(process_roots: List[SpanNode],
             if _same_span_set(ps_child, net_parent, 'network_span_set') \
                 or _same_span_set(ps_child, net_parent, 'process_span_set'):
                 continue
-            if ps_child.agent_id == net_parent.agent_id and not net_parent.time_range_cover(ps_child):
-                continue
-            if net_parent.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL and ps_child.signal_source!= L7_FLOW_SIGNAL_SOURCE_OTEL and \
-                net_parent.get_response_duration() and net_parent.get_response_duration() < ps_child_response_duration:
-                continue
+            if net_parent.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL and ps_child.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL:
+                if ps_child.agent_id == net_parent.agent_id and not net_parent.time_range_cover(
+                        ps_child):
+                    continue
+                if net_parent.get_response_duration(
+                ) and net_parent.get_response_duration(
+                ) < ps_child_response_duration:
+                    continue
             if ps_index == net_parent.get_flow_index():
                 # 共享一个 s-p，则 ps_child 的 parent == net_parent 的 parent
                 continue
@@ -2324,11 +2331,14 @@ def _connect_process_and_networks(process_roots: List[SpanNode],
             if _same_span_set(ps_child, ps_parent, 'network_span_set') \
                 or _same_span_set(ps_child, ps_parent, 'process_span_set'):
                 continue
-            if ps_child.agent_id == ps_parent.agent_id and not ps_parent.time_range_cover(ps_child):
-                continue
-            if ps_parent.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL and ps_child.signal_source!= L7_FLOW_SIGNAL_SOURCE_OTEL and \
-                ps_parent.get_response_duration() and ps_parent.get_response_duration() < ps_child_response_duration:
-                continue
+            if ps_parent.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL and ps_child.signal_source != L7_FLOW_SIGNAL_SOURCE_OTEL:
+                if ps_child.agent_id == ps_parent.agent_id and not ps_parent.time_range_cover(
+                        ps_child):
+                    continue
+                if ps_parent.get_response_duration(
+                ) and ps_parent.get_response_duration(
+                ) < ps_child_response_duration:
+                    continue
             if ps_child_index == ps_parent.get_flow_index():
                 # 共享一个 c-p，则 ps_child 的 parent == ps_parent 的 parent
                 continue
@@ -2407,7 +2417,8 @@ def _connect_process_and_networks(process_roots: List[SpanNode],
                 # 网关透传 x_request_id 或透传 http header 中的 span_id
                 # 要求 parent 的所有 response_duration > child 最大的 response_duration
                 # 由于 network span set 内是按 c 端 agent 在前+ start_time 排序的，可以认为 net_child(root) 就是一组内时延最大的
-                if net_parent.get_response_duration() < net_child_response_duration:
+                if net_parent.get_response_duration(
+                ) < net_child_response_duration:
                     continue
                 else:
                     # 这里不要直接设置 parent，如果找到了满足条件的情况，都加入列表待处理
